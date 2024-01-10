c581719053dd3d3a4c60f5c841091ea0ab122f75
Taylor Blau <me@ttaylorr.com>
2024-01-10 12:53:34 -0500

    t5302: run expected-to-fail `index-pack`s with `--threads=1`
    For identical reasons as in the previous commit, apply the same
    treatment to expected-to-fail `index-pack` invocations in t5302 with
    `--threads=1`.
    
    (Note that this treatment only needs to be applied in tests which are
    expected to pass when built with the leak-checker enabled).
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>

--- t/t5302-pack-index.sh
+++ t/t5302-pack-index.sh
@@ -290,7 +290,7 @@ test_expect_success 'too-large packs report the breach' '
 	pack=$(git pack-objects --all pack </dev/null) &&
 	sz="$(test_file_size pack-$pack.pack)" &&
 	test "$sz" -gt 20 &&
-	test_must_fail git index-pack --max-input-size=20 pack-$pack.pack 2>err &&
+	test_must_fail git index-pack --threads=1 --max-input-size=20 pack-$pack.pack 2>err &&
 	grep "maximum allowed size (20 bytes)" err
 '
 
